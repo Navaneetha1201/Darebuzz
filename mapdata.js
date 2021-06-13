a2190b2d4412b7889bc95e298955578a4dbf24da
Navaneetha.M <67795564+Navaneetha1201@users.noreply.github.com>
2021-06-13 19:07:24 +0530

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,342 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   
+		width: "responsive", //or 'responsive'
+    background_color: "#000000",
+    background_transparent: "no",
+    border_color: "",
+    pop_ups: "detect",
+    
+		
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+	
+		location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "START"
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      description: "ISLAND",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      description: "Blessed with two beautiful rivers, Krishna and Godavari, Andhra Pradesh.The state is most visited for its world-famous Tirupati temple.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      description: "It is known for the tribal culture and the simplicity of their exquisite beauty. Among all the states in India, it is the most sparsely populated. Its amazing array of flora and fauna",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "4": {
+      name: "Assam",
+      description: "A treasure trove of natural beauty and diverse history, Assam is one of the least explored regions in India, lending it an immaculate, untouched aura that will allure you.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "5": {
+      name: "Bihar",
+      description: "A land where Buddha once lived, a land of monasteries, Bihar is visited by huge number of Buddhist followers as well as tourists of other faiths",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "6": {
+      name: "Chandigarh",
+      description: "Haryana is a vibrant state that has managed to retain the best of both worlds- the ancient yet vibrant past and an exciting future to look forward",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      description: "The oldest tribes of India dwell in Chhattisgarh, some of them for almost 10,000 years. A blend of culture, arts and religion of the local and tribal people, Chattisgarh exhibits the instance of ancient India.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      description: "Dadra and Nagar Haveli is a district of the Indian union territory of Dadra and Nagar Haveli and Daman and Diu in western India.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "9": {
+      name: "Daman And Diu",
+      description: "ISLAND",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "10": {
+      name: "Delhi",
+      description: "Delhi, India’s capital territory, is a massive metropolitan area in the country’s north. In Old Delhi, a neighborhood dating to the 1600s, stands the imposing Mughal-era Red Fort, a symbol of India, and the sprawling Jama Masjid mosque, whose courtyard accommodates 25,000 people. Nearby is Chandni Chowk, a vibrant bazaar filled with food carts, sweets shops and spice stalls",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "11": {
+      name: "Goa",
+      description: "Lying on the western coast, Goa is India's smallest state and unlike any other, known for its white-sand beaches, stellar nightlife, eclectic seafood, world-heritage listed architecture",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "12": {
+      name: "Gujarat",
+      description: "Home to several architectural marvels, Gujarat is famous for its vibrant culture and rich heritage apart from the scenic landscapes and mouth-watering cuisine.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "13": {
+      name: "Haryana",
+      description: "Haryana is a vibrant state that has managed to retain the best of both worlds- the ancient yet vibrant past and an exciting future to look forward",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      description: "A place of immense beauty and charm, Himachal is endowed with clear and serene lakes, lofty mountains, ancient temples and cheerful people who are as innocent as nature itself.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "16": {
+      name: "Jharkhand",
+      description: "One of the best places to visit for wildlife aficionados and nature lovers, Jharkhand is a state replete with a vast bounty of towering mountains, dense forests and gushing waterfalls.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "17": {
+      name: "Karnataka",
+      description: "Ranked the 4th most popular tourist state in India, Karnataka has a bouquet of attractions to offer you. Expanded from Belgaum in the North to Bangalore in the south which is also its capital",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "18": {
+      name: "Kerala",
+      description: "Kerala is famously known as 'God’s Own Country'. Whether you want to bask in the sun and sand of the palm tree-lined Kovalam beaches, or you want to take a hike to Munnar hill station, or even if you just want to walk down the bustling streets of Kochi",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "19": {
+      name: "Lakshadweep",
+      description: "ISLAND",
+      color: "ISLAND",
+      hover_color: "default",
+      url: "default"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      description: "Madhya Pradesh got its name because of its geographical position, being located in the centre of India. However, it's tagline 'The Heart of Incredible India' has more to do with the fact that it houses facets of the whole of India into this state.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "21": {
+      name: "Maharashtra",
+      description: "Often called the 'Gateway to the Heart of India', Maharashtra is quite larger than most states in India. Owing to its proximity to the Western Ghats, it is blessed with a picturesque backdrop of the mountains on one side and has the beautiful Konkan coastal belt on the other.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "22": {
+      name: "Manipur",
+      description: "The jewel city of India, Manipur is listed as one of the most beautiful states in India. Least touched and least discovered, Manipur promises to be the great tourist discovery as the state offers endless enjoyment and pleasure with its exotic landscape, undulating hills, green valleys, blue lakes and dense forests.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "23": {
+      name: "Meghalaya",
+      description: "Meghalaya, the abode of clouds, is one of the most beautiful states in the North-East India offering a variety of sights, activities, food and festivals to the tourists.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "24": {
+      name: "Mizoram",
+      description: "Known as the 'Songbird of India', the northeastern state of Mizoram is one of the smallest states in India. The moderate climate of Mizoram, which is comfortable even in during summer makes it an attractive destination for all the tourists.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "25": {
+      name: "Nagaland",
+      description: "Nicknamed as the ‘Switzerland of the East’, Nagaland is a quaint state in the north-east part of India and is one of the most-loved hill station tourist destinations.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "26": {
+      name: "Orissa",
+      description: "Land of paddy fields and palm fringed silver beaches, temples, rivers, waterfalls and tribal people, Odisha's major attractions are the temples of Bhubaneshwar and Puri, miles of clean silver beaches and the magnificent Sun Temple of Konark.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "27": {
+      name: "Puducherry",
+      description: "Puducherry, also known as Pondicherry, is a union territory of India. It was formed out of four territories of former French India, namely Pondichéry, Karikal, Mahé and Yanaon, excluding Chandannagar. ",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "28": {
+      name: "Punjab",
+      description: "Punjab literally means the land of five rivers. A network of five rivers - the Ravi, Beas, Sutlej, Chenab and Jhelum, irrigates this land and agriculture is the 'culture' of the state of Punjab. Known for its cuisine, culture and history Punjab is visited by tourists",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "29": {
+      name: "Rajasthan",
+      description: "Formerly known as the Land of the Kings, Rajasthan is a beautiful example of India’s age-old opulence and grandeur, traces of which still linger in the air of this state.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "30": {
+      name: "Sikkim",
+      description: "Sikkim, the smallest state of India, is the land of large variety of plants, animals, rivers, mountains, lakes and waterfalls. The mountain peaks, holy lakes, ancient monasteries, orchid nurseries and stunning trekking routes make Sikkim a perfect Holiday destination for you.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      description: "A land of cultural and religious heritage, Tamil Nadu offers every chance to the tourist or pilgrims to be enchanted with the vast plethora of history and beauty that it has. It is a blend of modern and ancient, that makes Tamil Nadu a beautiful destination to explore our past cultural heritage with all the amenities of the present.",
+      color: "default",
+      hover_color: "default",
+      url: "https://oviyaudhayakumar.github.io/gallary/"
+    },
+    "32": {
+      name: "Tripura",
+      description: "Rich in gorgeous landscapes, crystalline waterfalls, awe-inspiring mountains, dense forests, and a generous sprinkling of history and tradition, Tripura is a popular tourist destination situated in North-East India.",
+      color: "default",
+      hover_color: "default",
+      url: ""
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      description: "Home to one of the Seven Wonders of the World - Taj Mahal - and flaunting exquisite arts like the handicrafts and jewellery Uttar Pradesh is a bountiful state and an utterly religious one.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "34": {
+      name: "Uttaranchal",
+      description: "Famously known as Devbhumi, or the Land of the Gods, Uttarakhand is a land of untouched natural beauty and sublime spirituality. Carved out from Uttar Pradesh, the state, formerly known as Uttaranchal, is a place that not only boasts of a scintillating view of the Himalayas",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "35": {
+      name: "West Bengal",
+      description: "West Bengal boasts of different ethnicities, cultures, religions, people and languages which add to this beautiful landscape, forests, coastal beauty, heritage",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      description: "Jammu and Kashmir is a region administered by India as a union territory and consists of the southern portion of the larger Kashmir region.Jammu and Kashmir, officially known as the Princely State of Kashmir and Jammu, was a princely state during the British East India Company rule",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "37": {
+      name: "Telangana",
+      description: "Telangana is the youngest among the states in India, consisting of the ten north-western districts of Andhra Pradesh. It was originally part of the region comprising Hyderabad in Andhra Pradesh.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "38": {
+      name: "Ladakh",
+      description: "Ladakh is most famous for breathtaking landscapes, the crystal clear skies, the highest mountain passes, thrilling adventure activities, Buddhist Monasteries and festivals. ... Ladakh gains a lot of popularity for being the only cold desert in India apart from bordering the World's highest saltwater Lake Pangong Lake.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    standard: {
+      color: "#cecece",
+      inactive: "yes"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 18.987807,
+      lng: 72.836447,
+      name: "Mumbai"
+    }
+  },
+  labels: {}
+};
